3b6be7d4cb0080d2711eacf9dc21c066f4c2434d
Tyler Wright <tyler.wright@fullstackacademy.com>
2024-03-18 14:03:00 -0700

    solutions

---
+++ 10-DOM/01-Traverse-DOM/02-Activity/Solved/assets/js/script.js
@@ -0,0 +1,7 @@
+// Access element using id
+const articlesDiv = document.getElementById("articles");
+const mainDiv = document.getElementById("main");
+
+// Change style by accessing style object's properties
+articlesDiv.children[0].style.fontSize = "50px";
+mainDiv.children[1].style.color = "white";
